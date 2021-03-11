f81a3c1fe35bda6caa885c53d8ac7e406d724a73
Ghoulbot-hue <gustagc06@gmail.com>
2021-03-11 04:31:31 -0300

    first commit

---
+++ darkbot3.0-main/Komputer/suporte.java
@@ -0,0 +1,5 @@
+Pc = komputer * (Ghoulbot3.0 + Ghoulbot - Ghoul)
+           + 4 * bot; // PREFER
+
+Android = /0/android/suporte.java * (Ghoulbot3.0 + ghoul
+                       - Ghoulbot) + 4 * bot; // AVOID
---
+++ darkbot3.0-main/interface/name.php
@@ -0,0 +1,3 @@
+___| Ferramenta By __|
+|  Ghoul__|
+|__________|
